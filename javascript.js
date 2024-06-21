804267d96d6d5d0e26c850010c0c234d72eddbe6
mayurithotange <mayurithotange@gmail.com>
2024-06-21 15:23:22 -0500

    changes

--- javascript.js
+++ javascript.js
@@ -1,22 +1,22 @@
-const per = parseInt(prompt(`Enter your percentage`));
 
+const per = parseInt(prompt('Enter Percentage:'));
 
-if (per >= 90)
-     {
-    document.write("Grade: A");
-} 
-else if (per>= 80)
-     {
-    document.write("Grade: B");
+if (per >= 50 && per < 60) {
+    document.write(' Grade:E')
 }
- else if (per>= 70) 
-    {
-    document.write("Grade: C");
+else if (per >= 60 && per < 70) {
+    document.write(' Grade:D')
 }
- else if (per >= 60)
-     {
-    document.write("Grade: D");
-} 
+else if (per >= 70 && per < 80) {
+    document.write(' Grade:C')
+}
+else if (per >= 80 && per < 90) {
+    document.write(' Grade:B')
+}
+else if (per >= 90 && per <= 100) {
+   document.write(' Grade:A')
+}
 else {
-    document.write("Grade: F");
+    console.log('Fail')
 }
+
